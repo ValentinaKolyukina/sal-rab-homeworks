9abbe19bf9aa3789d77481592b357ec6e4f8170d
Valentina Kolyukina <VKolyukina@inwk.com>
2021-05-20 10:23:17 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,6 +1,15 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message;
+    message = productName;
+    message = message + " ";
+    message = message + "за";
+    message = message + " ";
+    message = message + productPrice;
+    message = message + " ";
+    message = message + "теперь в корзине!";
+    console.log(message);
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
@@ -23,6 +32,8 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue;
+    newValue = oldValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,7 +47,11 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
-
+    let newSum;
+    newSum = oldSum + difference;
+    let newSumText;
+    newSumText = newSum + "₽";
+    newSumText = '$(newSum)₽';
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
