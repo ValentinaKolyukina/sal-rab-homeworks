25dc838702a4f2ef9766d2dbe65fa4a97ee06c38
Valentina Kolyukina <VKolyukina@inwk.com>
2021-06-07 21:28:07 +0300

    add Final task

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,22 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {
+        data: {
+            client: name + " " + phone,
+            order: { address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat, sum: sum },
+            goods: []
+        }
+    };
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i++) {
+        let obj = { title: goods[i].title, count: goods[i].count};
+        data.data.goods.push(obj);
+
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+   let jsonData = JSON.stringify(data);
 
     return jsonData;
 }
